afbd377370ef67f6112cffea03eeeb818c2b2768
mohsens1997 <42263386+mohsens1997@users.noreply.github.com>
2020-12-01 15:16:21 +0330

    Update creds.py

--- creds.py
+++ creds.py
@@ -8,10 +8,10 @@ class Creds():
     # Because of pydrive And pydrive v2 Api
     
     #Folder Id Of Teamdrive
-    TEAMDRIVE_FOLDER_ID = ""
+    TEAMDRIVE_FOLDER_ID = "1vvD9aNsRHlWF1r_9-9Ek8tBu-EdNdup_"
     
     # Id of Team drive 
-    TEAMDRIVE_ID = ""
+    TEAMDRIVE_ID = "0AMmZXGsYx5GaUk9PVA"
     
     
     
